6416e699291247e848a3d932a48cba96e0b6db5c
gabrielvf64 <gabrielvf64@gmail.com>
2019-10-12 22:00:51 -0300

    [129] Controlando cadastro de cliente com FormGroup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+    this.formGroup = this.formBuilder.group({
+      nome: ['Exemplo', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['exemplo@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['01004692056', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua', [Validators.required]],
+      numero: ['100', [Validators.required]],
+      complemento: ['apart 1101', []],
+      bairro: ['Santa Rosa', []],
+      cep: ['10828333', [Validators.required]],
+      telefone1: ['998476550', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
+    });
   }
 
   signupUser() {
     console.log("enviou o form");
   }
-
 }
