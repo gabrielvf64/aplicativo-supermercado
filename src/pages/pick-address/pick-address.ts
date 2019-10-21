575ad62b729a66cbd4d455cfe05bd8f6f8751b4b
gabrielvf64 <gabrielvf64@gmail.com>
2019-10-20 23:00:06 -0300

    [144] Obtendo endereços do objeto cliente para carregar na tela de escolha do endereço de entrega

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -11,43 +13,26 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Gastão Gonçalves",
-        numero: "79",
-        complemento: "Bloco A",
-        bairro: "Santa Rosa",
-        cep: "24240030",
-        cidade: {
-          id: "1",
-          nome: "Niterói",
-          estado: {
-            id: "1",
-            nome: "Rio de Janeiro"
-          }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Doze",
-        numero: "10",
-        complemento: null,
-        bairro: "Itaipuaçu",
-        cep: "24900000",
-        cidade: {
-          id: "2",
-          nome: "Maricá",
-          estado: {
-            id: "1",
-            nome: "Rio de Janeiro"
-          }
-        }
-      }
-    ];
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          console.log('response', response)
+          this.items = response['enderecos'];
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 }
